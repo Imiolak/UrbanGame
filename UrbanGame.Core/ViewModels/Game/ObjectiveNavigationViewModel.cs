8bb9d6ff57af915c4e27bf99accfc9b2811c3218
Imiolak <maciej.imiolek@gmail.com>
2017-08-16 17:15:47 +0200

    Objective navigation fragment with view model

---
+++ UrbanGame.Core/ViewModels/Game/ObjectiveNavigationViewModel.cs
@@ -0,0 +1,42 @@
+﻿using MvvmCross.Core.ViewModels;
+using UrbanGame.Core.Services;
+
+namespace UrbanGame.Core.ViewModels.Game
+{
+    public class ObjectiveNavigationViewModel : MvxViewModel
+    {
+        private readonly IApplicationVariableService _applicationVariableService;
+
+        public ObjectiveNavigationViewModel(IApplicationVariableService applicationVariableService)
+        {
+            _applicationVariableService = applicationVariableService;
+        }
+
+        private int _currentObjective = 1;
+        public int CurrentObjective
+        {
+            get => _currentObjective;
+            set
+            {
+                _currentObjective = value;
+                RaisePropertyChanged(nameof(CurrentObjective));
+            }
+        }
+
+        public int NumberOfObjectives => 10;
+
+        public MvxCommand PreviousObjectiveCommand => new MvxCommand(PreviousObjective);
+        
+        public MvxCommand NextObjectiveCommand => new MvxCommand(NextObjective);
+
+        private void PreviousObjective()
+        {
+            CurrentObjective--;
+        }
+
+        private void NextObjective()
+        {
+            CurrentObjective++;
+        }
+    }
+}
---
+++ UrbanGame.Droid/Views/Game/ObjectiveNavigationFragment.cs
@@ -0,0 +1,19 @@
+﻿using Android.OS;
+using Android.Views;
+using MvvmCross.Binding.Droid.BindingContext;
+using MvvmCross.Droid.Support.V4;
+using MvvmCross.Platform;
+using UrbanGame.Core.ViewModels.Game;
+
+namespace UrbanGame.Droid.Views.Game
+{
+    public class ObjectiveNavigationFragment : MvxFragment<ObjectiveNavigationViewModel>
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+            ViewModel = Mvx.IocConstruct<ObjectiveNavigationViewModel>();
+            return this.BindingInflate(Resource.Layout.fragment_objectivenavigation, null);
+        }
+    }
+}
